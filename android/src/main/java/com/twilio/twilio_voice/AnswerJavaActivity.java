9a0d6b0f432b59b42b5e4d5a24c937484832d6f8
pisipisi <thanhsnguyen@hotmail.com>
2021-12-21 14:01:52 -0600

    update unknow

--- android/src/main/java/com/twilio/twilio_voice/AnswerJavaActivity.java
+++ android/src/main/java/com/twilio/twilio_voice/AnswerJavaActivity.java
@@ -152,7 +152,7 @@ public class AnswerJavaActivity extends AppCompatActivity {
 
             String fromId = activeCallInvite.getFrom().replace("client:", "");
             SharedPreferences preferences = getApplicationContext().getSharedPreferences(TwilioPreferences, Context.MODE_PRIVATE);
-            String caller = preferences.getString(fromId, preferences.getString("defaultCaller", getString(R.string.unknown_caller)));
+            String caller = preferences.getString(fromId, preferences.getString("defaultCaller", fromId));
             tvUserName.setText(caller);
 
             btnAnswer.setOnClickListener(new View.OnClickListener() {
